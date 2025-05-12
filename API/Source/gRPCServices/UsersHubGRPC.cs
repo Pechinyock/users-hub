5ccbc26f7f5471f97f14d5f260c2d615b6e3d7c9
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-05-12 18:01:58 +0300

    - delete unused property

--- API/Source/gRPCServices/UsersHubGRPC.cs
+++ API/Source/gRPCServices/UsersHubGRPC.cs
@@ -14,14 +14,12 @@ internal sealed class UsersHubGRPC : Contracts.UserHub.UserHubBase
 
     public override async Task<CreateUserResponse> Create(CreateUserRequest request, ServerCallContext context)
     {
-        var login = request.Login;
         var model = request.ToModel();
 
         await _userHubService.CreateUserAsync(model);
         var result = Task.FromResult(new CreateUserResponse()
         {
-            Login = login,
-            IsSuccess = true
+            Login = request.Login
         }).Result;
 
         return result;
